3681edf2ae55a3a4f0d4308fbcd16b7220e7d63c
Sourabh Shankar Patil <sourabhpatil2611@gmail.com>
2024-09-19 19:35:58 +0530

    changed api domain name

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://blog-diary-api.onrender.com/',
         secure: false,
       },
     },
