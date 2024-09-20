83035094b92c70b7fdbbfbb51f0527dd9b2d3ba5
Sourabh Shankar Patil <sourabhpatil2611@gmail.com>
2024-09-20 11:14:19 +0530

    chnaged api domain

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://blog-diary-api.onrender.com/',
-        secure: false,
+        changeOrigin: true,
+        secure: true, //becuase using https
       },
     },
   },
